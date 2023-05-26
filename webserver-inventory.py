75ac10eade6211f5f42093eea0ea5673d40f9f35
Sami Mnif <samimnif@icloud.com>
2023-05-26 13:15:21 -0400

    Added more features on the loaning page. Added shopping list and cart functions. Edited the add, adjust and index templates.

--- webserver-inventory.py
+++ webserver-inventory.py
@@ -7,6 +7,19 @@ from datetime import datetime
 app = Flask(__name__)
 inventory = []
 loaner = []
+shopping = []
+
+def save_shop():
+    global shopping
+    with open('shopping.json', 'w') as file:
+        json.dump(shopping, file, indent=4)
+
+def load_shop():
+    try:
+        with open('shopping.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
 
 def save_loan():
     global loaner
@@ -43,11 +56,21 @@ def generate_unique_filename(filename):
     unique_filename = f'{timestamp}_{unique_id}{extension}'
     return unique_filename
 
+def unpurchased_items():
+    global shopping
+    shopping = load_shop()
+    count = 0
+    for item in shopping:
+        if item['purchased'] == "no":
+            count += 1
+    return count
+
 @app.route('/')
 def index():
-    global inventory
+    global inventory, shopping
     inventory = load_inventory()
-    return render_template('index.html', inventory=inventory)
+    shopping = load_shop()
+    return render_template('index.html', inventory=inventory, shopItems=unpurchased_items())
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_item():
@@ -91,7 +114,7 @@ def adjust_quantity(item_name):
             print(inventory)
             save_inventory()
             return redirect(url_for('index'))
-        return render_template('adjust.html', item=item)
+        return render_template('adjust.html', item=item, tags = load_config()["tags"])
     return redirect(url_for('index'))
 
 @app.route('/remove', methods=['GET', 'POST'])
@@ -119,10 +142,13 @@ def record_loan():
     if request.method == 'POST':
         loaner_name = request.form['loaner_name']
         loaned_item = request.form['loaned_item']
-
+        item = next((item for item in inventory if item['item_name'] == loaned_item), None)
+        item['quantity'] -= 1
+        print(inventory)
+        save_inventory()
         # Save the loan object to loaner.json
         loaner = load_loan()
-        loaner.append({"loaner_name": loaner_name, "loaned_item": loaned_item})
+        loaner.append({"loaner_name": loaner_name, "date": datetime.now().strftime('%Y-%m-%d @ %H:%M:%S'), "loaned_item": loaned_item})
         save_loan()
         return redirect(url_for('index'))
 
@@ -136,6 +162,34 @@ def loaned_page():
     print(inventory)
     print(loaner)
     return render_template('loaned.html', loaned_items=loaner, items=inventory)
+
+@app.route('/shopping', methods=['GET', 'POST'])
+def shopping():
+    global shopping
+    shopping = load_shop()
+    if request.method == 'POST':
+        item = request.form['item']
+        unique_id = str(uuid.uuid4().hex)
+        link = request.form['link']
+        quantity = request.form['quantity']
+        shopping.append({'item': item, 'link': link, 'quantity': quantity, 'id': unique_id, 'purchased': "no"})
+        save_shop()
+        return redirect(url_for('shopping'))
+    
+    return render_template('shopping.html', shopping_list=shopping)
+@app.route('/purchase', methods=['POST'])
+def purchase():
+    global shopping
+    shopping = load_shop()
+    if request.method == 'POST':
+        itemID = request.form['item_id']
+        for item in shopping:
+            if item['id'] == itemID:
+                item['purchased'] = "yes"
+                item['date'] = datetime.now().strftime('%Y-%m-%d @ %H:%M:%S')
+                save_shop()
+                break
+        return redirect(url_for('shopping'))
 
 if __name__ == '__main__':
     app.run(debug=True,host="172.17.26.2")
